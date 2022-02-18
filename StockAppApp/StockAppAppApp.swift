a3721f06e3bc2df89da70ec59256ee984bd7870a
novikovalexey88 <novikov.a.m@yandex.ru>
2022-02-19 00:09:19 +0300

    Initial Commit

---
+++ StockAppApp/StockAppAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppAppApp.swift
+//  StockAppApp
+//
+//  Created by 668 on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StockAppAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
